b56181c635cf21ec5ed41ea8e425d9ebfc473e2c
Oleksandr Shlinchak <ashlinchak@gmail.com>
2024-06-28 15:38:15 +0200

    Fix semantic-release configs

--- release.config.mjs
+++ release.config.mjs
@@ -1,13 +1,12 @@
-import github from '@semantic-release/github';
-import npm from '@semantic-release/npm';
-import commitAnalyzer from '@semantic-release/commit-analyzer';
-import releaseNotesGenerator from '@semantic-release/release-notes-generator';
-
-
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
   branches: ["main", "next"],
-  plugins: [commitAnalyzer, releaseNotesGenerator, github, npm],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/npm',
+  ],
 };
