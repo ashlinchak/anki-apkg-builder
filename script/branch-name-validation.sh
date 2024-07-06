ac65d8b923879d67aabca731eb38c89a018674c6
Oleksandr Shlinchak <ashlinchak@gmail.com>
2024-07-06 11:20:37 +0200

    Channge script for getting branch name

--- script/branch-name-validation.sh
+++ script/branch-name-validation.sh
@@ -1,12 +1,20 @@
+#!/bin/sh
+
 check_branch_name() {
   echo "⌛ Checking branch name..."
-  branchName=$(git symbolic-ref --short HEAD)
-  allowedPattern='^((feature|fix|release|chore|poc)\/[a-zA-Z0-9\-]+)$'
+  local branchName="$1"
+  local allowedPattern='^((feature|fix|release|chore|poc)\/[a-zA-Z0-9\-]+)$'
 
   if ! [[ $branchName =~ $allowedPattern ]]; then
-    echo "❌ Checking branch name failed"
+    echo "❌ Branch name \"$branchName\" is not allowed!"
     exit 1
   fi
 }
 
-check_branch_name
+if [ -z "$1" ]; then
+  echo "Error: Branch name is required."
+  exit 1
+fi
+
+branchName="$1"
+check_branch_name "$branchName"
